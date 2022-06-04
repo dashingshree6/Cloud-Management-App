165c2aebb3cbfe69a5f7c3710e8f0d65ca063705
dashingshree6 <dashingshree6@gmail.com>
2022-06-04 17:53:28 +0530

    Added Navbar component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,10 @@
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <div>
       <p className='text-4xl text-center'>Cloud Management</p>
+      <Navbar/>
     </div>
   );
 }
