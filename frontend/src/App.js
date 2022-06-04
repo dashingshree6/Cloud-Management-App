eb499a3ea610fd50b3aaeb011c81b89eef777db6
dashingshree6 <dashingshree6@gmail.com>
2022-06-04 19:08:50 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,22 @@
 import Navbar from "./components/Navbar";
+import About from './components/About';
+import AllInOne from './components/AllInOne';
+import Footer from './components/Footer';
+import Hero from './components/Hero';
+import Pricing from './components/Pricing';
+import Support from './components/Support';
 
 function App() {
   return (
     <div>
       <p className='text-4xl text-center'>Cloud Management</p>
       <Navbar/>
+      <Hero/>
+      <About/>
+      <Support/>
+      <AllInOne/>
+      <Pricing/>
+      <Footer/>
     </div>
   );
 }
